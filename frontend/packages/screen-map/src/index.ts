e2b68db9f71e7770364203fedeff795c5dd2e26b
martin-janci <2478078+martin-janci@users.noreply.github.com>
2026-05-07 19:58:22 +0200

    chore(screen-map): bootstrap @ppt/screen-map package skeleton

---
+++ frontend/packages/screen-map/src/index.ts
@@ -0,0 +1,6 @@
+export * from './types.js';
+export * from './schema.js';
+export { parseScreenMap, parseScreenMapString } from './parse.js';
+export { writeScreenMap, writeScreenMapString } from './write.js';
+export { validateScreenMap, type ValidationIssue, type ValidationContext } from './validate.js';
+export { discoverScreenMaps } from './discover.js';
